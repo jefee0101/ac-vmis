e5833083ea5a2a50535ea9e556804dc1728594f3
Venus Fabrigas <venusfabrigas@Venuss-MacBook-Air.local>
2026-03-23 15:23:38 +0800

    Fix trusted proxy header constants

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -15,7 +15,15 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
-        $middleware->trustProxies(at: '*', headers: Request::HEADER_X_FORWARDED_ALL);
+        $middleware->trustProxies(
+            at: '*',
+            headers: Request::HEADER_X_FORWARDED_FOR
+                | Request::HEADER_X_FORWARDED_HOST
+                | Request::HEADER_X_FORWARDED_PORT
+                | Request::HEADER_X_FORWARDED_PROTO
+                | Request::HEADER_X_FORWARDED_PREFIX
+                | Request::HEADER_X_FORWARDED_AWS_ELB,
+        );
 
         $middleware->web(append: [
             HandleInertiaRequests::class,
